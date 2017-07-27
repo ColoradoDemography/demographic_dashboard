d3528fc8eacf57b017eaba091eb8b088d5bb07c2
TBleess <todd.bleess@gmail.com>
2017-07-27 14:34:41 -0600

    Fixing component change download

--- setup.R
+++ setup.R
@@ -177,8 +177,8 @@ components_d=function(fips, name){
            deaths=as.numeric(deaths),
            netmigration=as.numeric(netmigration),
            naturalIncrease=births-deaths)%>%
-    bind_cols(data.frame(County=rep(name, length(unique(county_profile$year)))))%>%
-    select(County, year, naturalIncrease, netMigration=netmigration)
+    bind_cols(data.frame(County=rep(name, length(unique(x$year)))))%>%
+    select(County, year, births, deaths, naturalIncrease, netMigration=netmigration)
   
   
   
